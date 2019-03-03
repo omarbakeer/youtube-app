1d9e5a9b93d2ad38b608cfcc2c935f41b860ed3a
Omar Bakier <omarbakeer@gmail.com>
2019-03-03 23:03:49 +0200

    [mobile-ui] adding the input element to the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 import { ReactComponent as YTMobileIcon } from '../images/youtube.svg'
 import { ReactComponent as SearchIcon } from '../images/search.svg'
+import { MdClose } from 'react-icons/md'
 import './head-bar.scss'
 
 export default class Header extends Component {
+  state = {
+    isSearching: false
+  }
   render() {
+    const { isSearching } = this.state
     return (
       <header className="head-bar">
         <a href="#!" className="head-bar__yt-icon">
@@ -12,7 +17,18 @@ export default class Header extends Component {
         </a>
         <div className="head-bar__title-icon-container">
           {/* Text or Input according to the user action */}
-          <h1 className="head-bar__title">Title</h1>
+          {isSearching ? (
+            <React.Fragment>
+              <input
+                type="text"
+                name="searchInput"
+                className="head-bar__input"
+              />
+              <MdClose className="head-bar__clear-input-icon" />
+            </React.Fragment>
+          ) : (
+            <h1 className="head-bar__title">Title</h1>
+          )}
           <a href="#!" className="head-bar__search-icon">
             <SearchIcon />
           </a>
